40d7300b1be02c3eeb015d5aa33a8f5e18d00d24
FLORES\OrikPC <orikworld@gmail.com>
2018-05-19 14:33:35 +0300

    - integrated AppCenter

--- DailyFit/SharedClient/DailyFitNative/App.xaml.cs
+++ DailyFit/SharedClient/DailyFitNative/App.xaml.cs
@@ -1,30 +1,40 @@
 ﻿using CommonServiceLocator;
 using DailyFitNative.AppStart;
+using DailyFitNative.Common.Constants;
 using DailyFitNative.Infrastructure.Core.Views.Implementations;
 using DailyFitNative.Infrastructure.DependencyInjection;
 using DailyFitNative.Utilities.Navigation;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Xamarin.Forms;
 
 namespace DailyFitNative
 {
 	public partial class App : Application
 	{
+		#region Constructors
+
 		public App ()
 		{
 			InitializeComponent();
-		    DependencyHelper.SetDependencies();
-		    ServiceLocator.SetLocatorProvider(() => DependencyManager.Instance.ServiceLocator);
+			DependencyHelper.SetDependencies();
 
-		    var startingPage = DependencyManager.Instance.ServiceLocator.GetInstance<Page>(ViewId.LoginPage.ToString());
-		    MainPage = new BaseNavigationPage(startingPage);
+			ServiceLocator.SetLocatorProvider(() => DependencyManager.Instance.ServiceLocator);
 
-		    DependencyHelper.SetNavigationInstance(MainPage.Navigation);
+			var startingPage = DependencyManager.Instance.ServiceLocator.GetInstance<Page>(ViewId.LoginPage.ToString());
+			MainPage = new BaseNavigationPage(startingPage);
 
-        }
+			DependencyHelper.SetNavigationInstance(MainPage.Navigation);
+		}
+
+		#endregion
+
+		#region Protected Methods
 
 		protected override void OnStart ()
 		{
-			// Handle when your app starts
+			StartAppCenter();
 		}
 
 		protected override void OnSleep ()
@@ -36,5 +46,16 @@ namespace DailyFitNative
 		{
 			// Handle when your app resumes
 		}
+
+		#endregion
+
+		#region Private Methods
+
+		private void StartAppCenter()
+		{
+			AppCenter.Start($"{CommonConstants.APP_CENTER_DROID_KEY}{CommonConstants.APP_CENTER_IOS_KEY}", typeof(Analytics), typeof(Crashes));
+		}
+
+		#endregion
 	}
 }
---
+++ DailyFit/SharedClient/DailyFitNative/Common/Constants/CommonConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyFitNative.Common.Constants
+{
+    public class CommonConstants
+    {
+		//AppCenter
+	    public const string APP_CENTER_DROID_KEY = "android=f8aa76e3-4df7-4255-a984-0cb70abff17f;";
+	    public const string APP_CENTER_IOS_KEY = "ios=90509bfb-980d-4b56-97bb-eb314a3a39ff";
+
+    }
+}
